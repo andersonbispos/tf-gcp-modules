54d8ce8b76b0c054ba527f80d026afb9fe2adc41
Anderson Bispo <andersonbispos@gmail.com>
2022-08-10 17:33:36 -0300

    commit modulo vpc

---
+++ vpc/outputs.tf
@@ -0,0 +1,6 @@
+output "gateway_ipv4" {
+  value = google_compute_network.vpc.self_link
+}
+output "self_link" {
+  value = google_compute_network.vpc.self_link
+}
---
+++ vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "name" {
+  type        = string
+  description = "Deverá ser adicionado um descrição para sua rede"
+}
+
+variable "description" {
+  type        = string
+  description = "Coloque um nome que aparecerá no dashboard para sua rede"
+  default = "VPC criada via modulo"
+}
+
+variable "auto_create_subnetworks" {
+  type        = bool
+  description = "Define se devem ser criadas as subnets. Default: false"
+  /* default     = false */
+}
