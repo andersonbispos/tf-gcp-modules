b5cb24ddc0a7a42ddbd345fb0d04f9953fa901f3
Anderson Bispo <andersonbispos@gmail.com>
2022-08-10 22:21:46 -0300

    output

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,6 +1,7 @@
-output "gateway_ipv4" {
-  value = google_compute_network.vpc.self_link
-}
 output "self_link" {
   value = google_compute_network.vpc.self_link
+}
+
+output "rede_gerenciada" {
+  value = google_compute_network.vpc.auto_create_subnetworks
 }
