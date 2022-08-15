66561340de1ac667ef68beb8359e90f20d1cda1c
Anderson Bispo <andersonbispos@gmail.com>
2022-08-15 10:44:49 -0300

    corrigindo o modulo da instancia

--- instancia/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_self_link" {
-  description = "vm_self_link"
-  value = google_compute_instance.vm_modelo.self_link
-}
-
-output "vm_name" {
-  description = "vm_name"
-  value = google_compute_instance.vm_modelo.name
-}
